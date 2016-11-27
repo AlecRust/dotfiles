7822a6638b0a972634921ab39ccd4bea6c128fb0
Alec Rust <me@alecrust.com>
2016-11-27 16:45:01 +0000

    Remove greenkeeper from `node.sh`

--- node.sh
+++ node.sh
@@ -1,9 +1,8 @@
 # Install global npm packages
 
 npm i -g cloudflare-cli
-npm i -g eslint
-npm i -g greenkeeper
 npm i -g gulp
+npm i -g eslint
 npm i -g jshint
 npm i -g npm-check-updates
 npm i -g nodemon
