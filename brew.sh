46c0ff6a1b4695a816600b59a69c861f7cfb0dd4
Alec Rust <me@alecrust.com>
2019-12-08 18:20:32 +0000

    Add rsync to brew.sh

--- brew.sh
+++ brew.sh
@@ -33,6 +33,7 @@ brew install homebrew/dupes/screen
 # Install other useful binaries.
 brew install yarn
 brew install tmux
+brew install rsync
 brew install speedtest_cli
 brew install redis
 brew install heroku
