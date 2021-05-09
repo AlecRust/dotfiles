c0279c9ba61cc7361e026c9aac2c6efee8fb88e3
Alec Rust <me@alecrust.com>
2021-05-09 16:00:46 +0100

    Cleanup cask list

--- cask.sh
+++ cask.sh
@@ -1,38 +1,36 @@
-# Install native apps
+# General
+brew install --cask 1password
+brew install --cask balenaetcher
+brew install --cask brave-browser
+brew install --cask discord
+brew install --cask google-backup-and-sync
+brew install --cask gpg-suite
+brew install --cask hiddenbar
+brew install --cask iina
+brew install --cask istat-menus
+brew install --cask iterm2
+brew install --cask ivpn
+brew install --cask kap
+brew install --cask mac2imgur
+brew install --cask monitorcontrol
+brew install --cask raycast
+brew install --cask rectangle
+brew install --cask slack
+brew install --cask spotify
+brew install --cask sweet-home3d
+brew install --cask the-unarchiver
+brew install --cask transmission
+brew install --cask zoom
 
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
-
-brew cask install 1password 2> /dev/null
-brew cask install appcleaner 2> /dev/null
-brew cask install bartender 2> /dev/null
-brew cask install dash 2> /dev/null
-brew cask install eve 2> /dev/null
-brew cask install evernote 2> /dev/null
-brew cask install firefox 2> /dev/null
-brew cask install google-drive 2> /dev/null
-brew cask install imageoptim 2> /dev/null
-brew cask install istat-menus 2> /dev/null
-brew cask install iterm2-beta 2> /dev/null
-brew cask install kaleidoscope 2> /dev/null
-brew cask install keepingyouawake 2> /dev/null
-brew cask install mac2imgur 2> /dev/null
-brew cask install macdrops 2> /dev/null
-brew cask install opera 2> /dev/null
-brew cask install picasa 2> /dev/null
-brew cask install rescuetime 2> /dev/null
-brew cask install renamer 2> /dev/null
-brew cask install shortcat 2> /dev/null
-brew cask install skype 2> /dev/null
-brew cask install slack 2> /dev/null
-brew cask install sonos 2> /dev/null
-brew cask install sourcetree 2> /dev/null
-brew cask install rectangle 2> /dev/null
-brew cask install spotify 2> /dev/null
-brew cask install the-unarchiver 2> /dev/null
-brew cask install tmate 2> /dev/null
-brew cask install transmission 2> /dev/null
-brew cask install transmit 2> /dev/null
-brew cask install viscosity 2> /dev/null
-brew cask install vlc 2> /dev/null
-brew cask install vmware-fusion 2> /dev/null
+# Development
+brew install --cask docker
+brew install --cask firefox
+brew install --cask gas-mask
+brew install --cask imageoptim
+brew install --cask kaleidoscope
+brew install --cask sketch
+brew install --cask sourcetree
+brew install --cask sublime-text
+brew install --cask tableplus
+brew install --cask transmit
+brew install --cask visual-studio-code
