4b8da50ddbceb2ee9c06b68112bf94faa145431b
Alec Rust <me@alecrust.com>
2021-05-09 18:38:12 +0100

    Add symlinks script

---
+++ symlinks.sh
@@ -0,0 +1,13 @@
+# Symlink dotfiles to Home directory
+
+ln -s $HOME/projects/personal/dotfiles/.aliases $HOME/.aliases
+ln -s $HOME/projects/personal/dotfiles/.curlrc $HOME/.curlrc
+ln -s $HOME/projects/personal/dotfiles/.editorconfig $HOME/.editorconfig
+ln -s $HOME/projects/personal/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/projects/personal/dotfiles/.gitignore_global $HOME/.gitignore_global
+ln -s $HOME/projects/personal/dotfiles/.hushlogin $HOME/.hushlogin
+ln -s $HOME/projects/personal/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/projects/personal/dotfiles/.wgetrc $HOME/.wgetrc
+ln -s $HOME/projects/personal/dotfiles/.zshrc $HOME/.zshrc
+
+ln -s $HOME/Google\ Drive/Apps/iTerm/tmuxinator $HOME/.config
