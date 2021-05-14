5d30cca4b6888422a653be98ff8d0b4b516c3356
Alec Rust <me@alecrust.com>
2021-05-14 23:34:45 +0100

    Dot

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-source ./scripts/symlinks.sh
-source ./scripts/brew.sh
-source ./scripts/zsh.sh
-source ./scripts/ruby.sh
-source ./scripts/node.sh
-source ./scripts/cask.sh
+source ../scripts/symlinks.sh
+source ../scripts/brew.sh
+source ../scripts/zsh.sh
+source ../scripts/ruby.sh
+source ../scripts/node.sh
+source ../scripts/cask.sh
 
 echo "== 🎉 dotfiles setup complete 🎉 =="
