7f2163f6ba203aea94cbc91db7a89cc4857965e0
Alec Rust <me@alecrust.com>
2021-05-14 16:08:07 +0100

    Move dotfiles to /dot

--- symlinks.sh
+++ symlinks.sh
@@ -1,13 +1,13 @@
 # Symlink dotfiles to Home directory
 
-ln -s $HOME/projects/personal/dotfiles/.aliases $HOME/.aliases
-ln -s $HOME/projects/personal/dotfiles/.curlrc $HOME/.curlrc
-ln -s $HOME/projects/personal/dotfiles/.editorconfig $HOME/.editorconfig
-ln -s $HOME/projects/personal/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/projects/personal/dotfiles/.gitignore_global $HOME/.gitignore_global
-ln -s $HOME/projects/personal/dotfiles/.hushlogin $HOME/.hushlogin
-ln -s $HOME/projects/personal/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/projects/personal/dotfiles/.wgetrc $HOME/.wgetrc
-ln -s $HOME/projects/personal/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/projects/personal/dotfiles/dots/.aliases $HOME/.aliases
+ln -s $HOME/projects/personal/dotfiles/dots/.curlrc $HOME/.curlrc
+ln -s $HOME/projects/personal/dotfiles/dots/.editorconfig $HOME/.editorconfig
+ln -s $HOME/projects/personal/dotfiles/dots/.gitconfig $HOME/.gitconfig
+ln -s $HOME/projects/personal/dotfiles/dots/.gitignore_global $HOME/.gitignore_global
+ln -s $HOME/projects/personal/dotfiles/dots/.hushlogin $HOME/.hushlogin
+ln -s $HOME/projects/personal/dotfiles/dots/.vimrc $HOME/.vimrc
+ln -s $HOME/projects/personal/dotfiles/dots/.wgetrc $HOME/.wgetrc
+ln -s $HOME/projects/personal/dotfiles/dots/.zshrc $HOME/.zshrc
 
 ln -s $HOME/Google\ Drive/Apps/iTerm/tmuxinator $HOME/.config
