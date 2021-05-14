6d90ce5c5cebfd428fade25f36b1514edfbe82bd
Alec Rust <me@alecrust.com>
2021-05-14 22:42:47 +0100

    Further sorting of brew.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -8,14 +8,16 @@ brew upgrade
 brew install coreutils
 brew install findutils
 brew install gnu-sed
-brew install zsh
-brew install wget
-brew install rsync
-brew install vim
-brew install grep
 brew install openssh
+brew install rsync
+brew install grep
+brew install wget
+brew install vim
+brew install zsh
 
 # Install development tools
+brew install git
+brew install diff-so-fancy
 brew install autoenv
 brew install tmux
 brew install awscli
@@ -26,28 +28,23 @@ brew install httpie
 brew install imagemagick
 brew install vips
 
+brew tap heroku/brew
+brew install heroku
+
+# Install Postgres and start boot service
+brew install postgresql
+brew services start postgresql
+
 # Install other utilities
 brew install speedtest_cli
 brew install thefuck
 brew install trash
 brew install qrencode
 
-# Tap and install Heroku CLI
-brew tap heroku/brew
-brew install heroku
-
-# Tap and install Postgres and start boot service
-brew install postgresql
-brew services start postgresql
-
 # Install version managers
 # Install NVM as Oh My Zsh plugin: https://github.com/lukechilds/zsh-nvm
 brew install rbenv
 brew install pyenv
-
-# Install Git and tools
-brew install git
-brew install diff-so-fancy
 
 # Install programming font
 brew tap homebrew/cask-fonts
