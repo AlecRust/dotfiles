9b374e3fe5c934b2e17a5dbb7f051cc9a6c8201d
Alec Rust <me@alecrust.com>
2021-05-14 23:35:50 +0100

    Full path

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-source ../scripts/symlinks.sh
-source ../scripts/brew.sh
-source ../scripts/zsh.sh
-source ../scripts/ruby.sh
-source ../scripts/node.sh
-source ../scripts/cask.sh
+source ~/projects/personal/dotfiles/scripts/symlinks.sh
+source ~/projects/personal/dotfiles/scripts/brew.sh
+source ~/projects/personal/dotfiles/scripts/zsh.sh
+source ~/projects/personal/dotfiles/scripts/ruby.sh
+source ~/projects/personal/dotfiles/scripts/node.sh
+source ~/projects/personal/dotfiles/scripts/cask.sh
 
 echo "== 🎉 dotfiles setup complete 🎉 =="
