61f5c6d700e11ab0217a7382774ef7ef1b01fe24
Alec Rust <me@alecrust.com>
2021-05-14 20:56:06 +0100

    Simplify zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -5,7 +5,7 @@ chsh -s "$(which zsh)"
 
 echo "== Installing Oh My Zsh =="
 
-# Clone Oh My Zsh to ~/.oh-my-zsh
+# Clone Oh My Zsh
 git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
 
 # Create ~/.zshrc symlink
@@ -13,22 +13,16 @@ ln -s ~/projects/personal/dotfiles/dots/.zshrc ~/.zshrc
 
 echo "== Installing Oh My Zsh plugins =="
 
-# Install alias-tips
+# Clone Oh My Zsh plugins
 git clone https://github.com/djui/alias-tips ~/.oh-my-zsh/custom/plugins/alias-tips
-
-# Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-# Install zsh-nvm
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-
-# Install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 echo "== Installing Spaceship ZSH =="
 
-# Install Spaceship ZSH
+# Clone Spaceship ZSH
 git clone https://github.com/denysdovhan/spaceship-prompt ~/.oh-my-zsh/custom/themes/spaceship-prompt
 
-# Create symlink for theme file
+# Create theme file symlink
 ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
