6e74b26455e3f26dfe6e8c70377d693162a1f14a
Alec Rust <me@alecrust.com>
2021-05-15 11:36:14 +0100

    Remove unused Vim stuff

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -7,7 +7,6 @@ ln -s ~/projects/personal/dotfiles/dots/.editorconfig ~/.editorconfig
 ln -s ~/projects/personal/dotfiles/dots/.gitconfig ~/.gitconfig
 ln -s ~/projects/personal/dotfiles/dots/.gitignore_global ~/.gitignore_global
 ln -s ~/projects/personal/dotfiles/dots/.hushlogin ~/.hushlogin
-ln -s ~/projects/personal/dotfiles/dots/.vimrc ~/.vimrc
 ln -s ~/projects/personal/dotfiles/dots/.wgetrc ~/.wgetrc
 
 # Symlink tmuxinator directory in Google Drive to Home directory
