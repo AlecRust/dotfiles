f32db6feca9c9a23896984bf9bb38f322b8508a4
Alec Rust <me@alecrust.com>
2021-05-15 20:40:53 +0100

    Tidy setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Ask for administrator password upfront
-sudo -v
-
 # Run all setup scripts
 source ~/projects/personal/dotfiles/scripts/ssh.sh
 source ~/projects/personal/dotfiles/scripts/brew.sh
@@ -14,4 +11,4 @@ source ~/projects/personal/dotfiles/scripts/vscode.sh
 # source ~/projects/personal/dotfiles/scripts/cask.sh
 source ~/projects/personal/dotfiles/scripts/macos.sh
 
-echo "== Completed dotfiles setup - please restart =="
+echo "✨ Completed setup. Please restart machine."
