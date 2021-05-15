1a13f02adc911ae42cff93d4e15ea3eb6bcd8a8f
Alec Rust <me@alecrust.com>
2021-05-15 13:56:43 +0100

    Remove jshint

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,6 +1,6 @@
 echo "== Updating npm and installing Yarn =="
 
-# Source NVM to make it available immediately
+# Source NVM to run now
 source ~/.nvm/nvm.sh
 
 # Install latest Node
@@ -15,13 +15,12 @@ echo "== Installing global npm packages =="
 # Install global packages
 yarn global add npm-check-updates
 yarn global add npm-check
+yarn global add changelog
 yarn global add retry-cli
 yarn global add splash-cli
 yarn global add gulp-cli
 yarn global add eslint
 yarn global add stylelint
-yarn global add jshint
 yarn global add nodemon
 yarn global add serve
 yarn global add yo
-yarn global add changelog
