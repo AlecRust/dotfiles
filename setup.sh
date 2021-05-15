6f4c8c5b3bbe7d30ffb1bdc85fbe3c4a676ae007
Alec Rust <me@alecrust.com>
2021-05-15 12:52:53 +0100

    Run macos last

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-source ~/projects/personal/dotfiles/scripts/macos.sh
 source ~/projects/personal/dotfiles/scripts/symlinks.sh
 source ~/projects/personal/dotfiles/scripts/brew.sh
 source ~/projects/personal/dotfiles/scripts/zsh.sh
 source ~/projects/personal/dotfiles/scripts/node.sh
 source ~/projects/personal/dotfiles/scripts/ruby.sh
 # source ~/projects/personal/dotfiles/scripts/cask.sh
+source ~/projects/personal/dotfiles/scripts/macos.sh
 
 echo "== Completed dotfiles setup - please restart =="
