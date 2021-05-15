95089a9fbe801fe3dc515f36855255e97c7e173e
Alec Rust <me@alecrust.com>
2021-05-15 12:29:43 +0100

    Run node.sh before ruby.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ source ~/projects/personal/dotfiles/scripts/macos.sh
 source ~/projects/personal/dotfiles/scripts/symlinks.sh
 source ~/projects/personal/dotfiles/scripts/brew.sh
 source ~/projects/personal/dotfiles/scripts/zsh.sh
-source ~/projects/personal/dotfiles/scripts/ruby.sh
 source ~/projects/personal/dotfiles/scripts/node.sh
+source ~/projects/personal/dotfiles/scripts/ruby.sh
 # source ~/projects/personal/dotfiles/scripts/cask.sh
 
 echo "== Completed dotfiles setup - please restart =="
