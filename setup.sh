22b4cfb14b34d3b8a5d567aa6256c2056d209479
Alec Rust <me@alecrust.com>
2021-05-16 13:30:10 +0100

    Enable all setup scripts

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ source ~/projects/personal/dotfiles/scripts/node.sh
 source ~/projects/personal/dotfiles/scripts/ruby.sh
 source ~/projects/personal/dotfiles/scripts/iterm2.sh
 source ~/projects/personal/dotfiles/scripts/vscode.sh
-# source ~/projects/personal/dotfiles/scripts/cask.sh
-# source ~/projects/personal/dotfiles/scripts/mas.sh
+source ~/projects/personal/dotfiles/scripts/cask.sh
+source ~/projects/personal/dotfiles/scripts/mas.sh
 source ~/projects/personal/dotfiles/scripts/macos.sh
-# source ~/projects/personal/dotfiles/scripts/ssh.sh
+source ~/projects/personal/dotfiles/scripts/ssh.sh
 
 echo "✨ Completed setup. Please restart machine."
