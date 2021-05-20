9957a4c0ad6c9ffbd32f0797e6521983c93c5fbd
Alec Rust <me@alecrust.com>
2021-05-20 18:33:14 +0100

    Tidy cask

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -3,26 +3,27 @@
 echo "==> 📜 Installing Homebrew Cask apps"
 
 # General
-brew install --cask 1password
-brew install --cask balenaetcher
-brew install --cask brave-browser
-brew install --cask discord
 brew install --cask google-backup-and-sync
-brew install --cask gpg-suite
-brew install --cask hiddenbar
-brew install --cask iina
+brew install --cask monitorcontrol
+brew install --cask the-unarchiver
+brew install --cask brave-browser
+brew install --cask transmission
+brew install --cask balenaetcher
 brew install --cask istat-menus
+brew install --cask 1password
+brew install --cask gpg-suite
+brew install --cask mac2imgur
+brew install --cask hiddenbar
+brew install --cask rectangle
+brew install --cask spotify
+brew install --cask discord
+brew install --cask slack
+brew install --cask zoom
+brew install --cask iina
 brew install --cask ivpn
 brew install --cask kap
-brew install --cask mac2imgur
-brew install --cask monitorcontrol
-brew install --cask rectangle
-brew install --cask slack
-brew install --cask spotify
-brew install --cask sweet-home3d
-brew install --cask the-unarchiver
-brew install --cask transmission
-brew install --cask zoom
+
+# Install last so it enables extensions for other apps
 brew install --cask raycast
 
 # Development
