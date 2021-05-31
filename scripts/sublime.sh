97e1c03f05714db026ad8977b68999366f125352
Alec Rust <me@alecrust.com>
2021-05-31 13:34:05 +0100

    Add .php sublime text file association

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -23,4 +23,5 @@ duti -s com.sublimetext.4 public.data all
 
 duti -s com.sublimetext.4 .json all
 duti -s com.sublimetext.4 .yml all
+duti -s com.sublimetext.4 .php all
 duti -s com.sublimetext.4 .sql all
