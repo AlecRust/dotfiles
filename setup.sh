32c506e8d662f83fdb3c395a442fc2ea23a843de
Alec Rust <me@alecrust.com>
2021-06-06 11:08:16 +0100

    Fix paths

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@
 # shellcheck source=/dev/null
 
 # Run all setup scripts
-. ./dotfiles/scripts/brew.sh
-. ./dotfiles/scripts/shell.sh
-. ./dotfiles/scripts/node.sh
-. ./dotfiles/scripts/ruby.sh
-. ./dotfiles/scripts/iterm2.sh
-. ./dotfiles/scripts/vscode.sh
-. ./dotfiles/scripts/sublime.sh
-. ./dotfiles/scripts/cask.sh
-. ./dotfiles/scripts/mas.sh
-. ./dotfiles/scripts/macos.sh
+. ./scripts/brew.sh
+. ./scripts/shell.sh
+. ./scripts/node.sh
+. ./scripts/ruby.sh
+. ./scripts/iterm2.sh
+. ./scripts/vscode.sh
+. ./scripts/sublime.sh
+. ./scripts/cask.sh
+. ./scripts/mas.sh
+. ./scripts/macos.sh
 
 echo "✨ Completed setup - please restart machine"
