5866e76cb327fcef78093802dce331e69a88dd08
Alec Rust <me@alecrust.com>
2021-06-07 10:49:50 +0100

    Add CSV Sublime Text association

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -25,4 +25,5 @@ duti -s com.sublimetext.4 .json all
 duti -s com.sublimetext.4 .lock all
 duti -s com.sublimetext.4 .yml all
 duti -s com.sublimetext.4 .php all
+duti -s com.sublimetext.4 .csv all
 duti -s com.sublimetext.4 .sql all
