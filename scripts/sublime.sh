b08aa09f019f135fe5c4f063781c89dae95f8173
Alec Rust <me@alecrust.com>
2021-07-13 16:37:56 +0100

    Add Sublime Text .svg file type association

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -27,5 +27,6 @@ duti -s com.sublimetext.4 .yml all
 duti -s com.sublimetext.4 .css all
 duti -s com.sublimetext.4 .php all
 duti -s com.sublimetext.4 .csv all
+duti -s com.sublimetext.4 .svg all
 duti -s com.sublimetext.4 .sql all
 duti -s com.sublimetext.4 .rb all
