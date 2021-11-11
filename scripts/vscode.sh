87247abdcaf8d2d72cd912d54457db26e275a46a
Alec Rust <me@alecrust.com>
2021-11-11 13:09:48 +0000

    Add GitHub Copilot vscode extension

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -12,6 +12,7 @@ code --install-extension eamodio.gitlens
 code --install-extension mhutchie.git-graph
 code --install-extension EditorConfig.EditorConfig
 code --install-extension GitHub.vscode-pull-request-github
+code --install-extension GitHub.copilot
 code --install-extension keesschollaart.vscode-home-assistant
 code --install-extension mikestead.dotenv
 code --install-extension ninoseki.vscode-gem-lens
