87d09356cf8cb110b2a01011ef47f543cc5dbd93
Alec Rust <me@alecrust.com>
2022-03-26 14:00:22 +0000

    Associate .env files with Sublime Text

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -23,6 +23,7 @@ duti -s com.sublimetext.4 public.data all
 
 duti -s com.sublimetext.4 .json all
 duti -s com.sublimetext.4 .lock all
+duti -s com.sublimetext.4 .env all
 duti -s com.sublimetext.4 .yml all
 duti -s com.sublimetext.4 .css all
 duti -s com.sublimetext.4 .php all
