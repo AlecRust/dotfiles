602074f8d01eb4e450071d9372932a21ba24c9a8
Alec Rust <me@alecrust.com>
2022-03-27 12:09:17 +0100

    Replace Dotenv with Direnv vscode extension

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -14,7 +14,7 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension GitHub.vscode-pull-request-github
 code --install-extension GitHub.copilot
 code --install-extension keesschollaart.vscode-home-assistant
-code --install-extension mikestead.dotenv
+code --install-extension mkhl.direnv
 code --install-extension ninoseki.vscode-gem-lens
 code --install-extension shyykoserhiy.vscode-spotify
 code --install-extension streetsidesoftware.code-spell-checker
