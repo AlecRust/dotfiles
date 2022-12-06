cd7894a218be61bb36d33c1e05f8259767e86191
Alec Rust <me@alecrust.com>
2022-12-06 15:02:51 +0000

    Switch to non-mail GPG brew

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -11,7 +11,7 @@ brew install --cask google-drive
 brew install --cask appcleaner
 brew install --cask 1password
 brew install --cask 1password-cli
-brew install --cask gpg-suite
+brew install --cask gpg-suite-no-mail
 brew install --cask mac2imgur
 brew install --cask hiddenbar
 brew install --cask gas-mask
