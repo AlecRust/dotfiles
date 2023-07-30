9c958e072567621f8f277aa02ac5cf37cd65db5c
Alec Rust <me@alecrust.com>
2023-07-30 17:24:22 +0100

    Use sh to run setup scripts

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# shellcheck disable=SC1090
-
 REPO_PATH=$(cd "$(dirname "$0")" && pwd)
 
 # Run all setup scripts
-"$REPO_PATH/scripts/brew.sh"
-"$REPO_PATH/scripts/shell.sh"
-"$REPO_PATH/scripts/vscode.sh"
-"$REPO_PATH/scripts/sublime.sh"
-"$REPO_PATH/scripts/cask.sh"
-"$REPO_PATH/scripts/mas.sh"
-"$REPO_PATH/scripts/macos.sh"
+sh "$REPO_PATH/scripts/brew.sh"
+sh "$REPO_PATH/scripts/shell.sh"
+sh "$REPO_PATH/scripts/vscode.sh"
+sh "$REPO_PATH/scripts/sublime.sh"
+sh "$REPO_PATH/scripts/cask.sh"
+sh "$REPO_PATH/scripts/mas.sh"
+sh "$REPO_PATH/scripts/macos.sh"
 
 echo "✨ Setup complete - please restart machine"
