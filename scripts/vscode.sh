8bc1df50ef6b69c2a9e4b2cbe96885f8fb6f9ffe
Alec Rust <me@alecrust.com>
2023-10-14 17:02:10 +0100

    Tidy vscode extensions

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -22,10 +22,10 @@ code --install-extension GitHub.vscode-github-actions
 code --install-extension DanielSanMedium.dscodegpt
 code --install-extension keesschollaart.vscode-home-assistant
 code --install-extension shyykoserhiy.vscode-spotify
-code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension NuclleaR.vscode-extension-auto-import
 code --install-extension wix.vscode-import-cost
 code --install-extension vincaslt.highlight-matching-tag
+code --install-extension formulahendry.auto-rename-tag
 code --install-extension WakaTime.vscode-wakatime
 code --install-extension gruntfuggly.todo-tree
 
@@ -36,15 +36,15 @@ code --install-extension bmewburn.vscode-intelephense-client
 code --install-extension bierner.lit-html
 code --install-extension idleberg.applescript
 code --install-extension redhat.vscode-yaml
-code --install-extension Prisma.prisma
+code --install-extension tamasfe.even-better-toml
 code --install-extension bradlc.vscode-tailwindcss
-code --install-extension csstools.postcss
 code --install-extension Vue.volar
 code --install-extension Vue.vscode-typescript-vue-plugin
+code --install-extension Prisma.prisma
 code --install-extension Tobermory.es6-string-html
-code --install-extension tamasfe.even-better-toml
 
 # Linting
+code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
@@ -52,7 +52,3 @@ code --install-extension stylelint.vscode-stylelint
 code --install-extension HTMLHint.vscode-htmlhint
 code --install-extension kokororin.vscode-phpfmt
 code --install-extension timonwong.shellcheck
-
-# Themes
-code --install-extension enkia.tokyo-night
-code --install-extension PenumbraTheme.penumbra
