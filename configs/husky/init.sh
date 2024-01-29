d947a25c1524238c75d4da4762468446e4e96aaa
Alec Rust <me@alecrust.com>
2024-01-29 14:34:00 +0000

    Disable Shellcheck rule in husky init.sh

--- configs/husky/init.sh
+++ configs/husky/init.sh
@@ -1,3 +1,5 @@
+# shellcheck disable=SC2148
+
 # Runs before husky hooks, in non-interative shell environments like Sourcetree
 # https://typicode.github.io/husky/how-to.html#node-version-managers-and-guis
 
